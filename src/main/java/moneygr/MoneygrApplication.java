c8a66e9c0b584566da4a07b5c568feead3c69cc8
Toshiaki Maki <makingx@gmail.com>
2015-09-07 03:09:03 +0900

    Polish

--- src/main/java/moneygr/MoneygrApplication.java
+++ src/main/java/moneygr/MoneygrApplication.java
@@ -1,13 +1,16 @@
 package moneygr;
 
-import moneygr.domain.repository.user.UserRepository;
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+
 import moneygr.thrift.TUserService;
 
-import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TJSONProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
 import org.apache.thrift.server.TServlet;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
@@ -15,30 +18,12 @@ import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Collections;
 
 @SpringBootApplication
 public class MoneygrApplication {
 
 	@Bean
-	public CommandLineRunner runner(UserRepository userRepository,
-			PlatformTransactionManager transactionManager) {
-		return (args) -> {
-			new TransactionTemplate(transactionManager).execute(tx -> {
-				userRepository.findAll().forEach(x -> {
-					System.out.println(x);
-				});
-				return null;
-			});
-		};
-	}
-	@Bean FilterRegistrationBean crosFilter() {
+	FilterRegistrationBean crosFilter() {
 		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
 		registrationBean.setUrlPatterns(Collections.singleton("/*"));
 		registrationBean.setFilter(new Filter() {
@@ -50,13 +35,14 @@ public class MoneygrApplication {
 			@Override
 			public void doFilter(ServletRequest servletRequest,
 					ServletResponse servletResponse, FilterChain filterChain)
-					throws IOException, ServletException {
+							throws IOException, ServletException {
 				HttpServletResponse response = (HttpServletResponse) servletResponse;
 				response.setHeader("Access-Control-Allow-Origin", "*");
 				response.setHeader("Access-Control-Allow-Methods",
 						"POST, GET, OPTIONS, PUT, DELETE");
 				response.setHeader("Access-Control-Max-Age", "3600");
-				response.setHeader("Access-Control-Allow-Headers", "*");
+				response.setHeader("Access-Control-Allow-Headers",
+						"Origin,X-Requested-With,Content-Type,Accept,Authorization");
 				filterChain.doFilter(servletRequest, servletResponse);
 			}
 
@@ -67,6 +53,7 @@ public class MoneygrApplication {
 		});
 		return registrationBean;
 	}
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		// for backward compatibility
