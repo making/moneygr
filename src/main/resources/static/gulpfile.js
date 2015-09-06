1985e02d8168ad840315b77ec6febc0532f2c6c1
Toshiaki Maki <makingx@gmail.com>
2015-09-06 21:21:13 +0900

    Add Pivotal UI

--- src/main/resources/static/gulpfile.js
+++ src/main/resources/static/gulpfile.js
@@ -2,6 +2,7 @@ var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var babelify = require('babelify');
+var drFrankenstyle = require('dr-frankenstyle');
 
 gulp.task('browserify', function () {
     return browserify('./src/index.js', {
@@ -11,6 +12,11 @@ gulp.task('browserify', function () {
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('./'));
+});
+
+gulp.task('css', function() {
+    return drFrankenstyle()
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function () {
--- src/main/resources/static/src/components.js
+++ src/main/resources/static/src/components.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {Thrift, TUserServiceClient} from './thrift-client.js';
+import {DefaultButton} from 'pui-react-buttons';
+import {Divider} from 'pui-react-dividers';
+import {DefaultH1, DefaultH2, DefaultH3, DefaultH4, DefaultH5, DefaultH6, AlternateH1, AlternateH2, AlternateH3, AlternateH4, AlternateH5, AlternateH6, MarketingH1, MarketingH2, MarketingH3, MarketingH4, MarketingH5, MarketingH6, Heading} from 'pui-react-typography'
+import {SortableTable, TableHeader, TableRow, TableCell} from 'pui-react-sortable-table';
 
 var transport = new Thrift.Transport("http://localhost:8080/user");
 var protocol = new Thrift.Protocol(transport);
@@ -8,45 +12,48 @@ var client = new TUserServiceClient(protocol);
 export default class HelloWorld extends React.Component {
     render() {
         return (
-            <div>
-                <h1>React.js Sample</h1>
+            <div className="container">
+                <DefaultH1>React.js Sample</DefaultH1>
+                <DefaultButton>Reload</DefaultButton>
+                <Divider />
                 <Display />
             </div>
         );
     }
 }
-
 class Display extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {users: []};
     }
 
     componentDidMount() {
-        client.findUser(1, (body) => {
-            this.setState(body);
+        client.findUsers((body) => {
+            this.setState({users: body});
         });
     }
 
     render() {
+        var body = this.state.users.map(function (user, key) {
+            return (
+                <TableRow key={key}>
+                    <TableCell>{user.userId}</TableCell>
+                    <TableCell>{user.email}</TableCell>
+                    <TableCell>{user.firstName}</TableCell>
+                    <TableCell>{user.lastName}</TableCell>
+                </TableRow>)
+        });
         return (
-            <table>
-                <tr>
-                    <th>UserId</th>
-                    <td>{this.state.userId}</td>
-                </tr>
-                <tr>
-                    <th>Email</th>
-                    <td>{this.state.email}</td>
-                </tr>
-                <tr>
-                    <th>FirstName</th>
-                    <td>{this.state.firstName}</td>
-                </tr>
-                <tr>
-                    <th>LastName</th>
-                    <td>{this.state.lastName}</td>
-                </tr>
-            </table>);
+            <SortableTable
+                headers={[
+                    <TableHeader sortable={true}>UserId</TableHeader>,
+                    <TableHeader sortable={true}>Email</TableHeader>,
+                    <TableHeader sortable={true}>FirstName</TableHeader>,
+                    <TableHeader sortable={true}>LastNAme</TableHeader>
+                  ]}
+                className="table-light">
+                {body}
+            </SortableTable>
+        );
     }
 }
